8c8e44377445eb54b464e6996882a0d5fd97c556
914813666 <com.qzp>
2015-05-05 15:41:52 +0800

    Initial Commit

---
+++ Jzb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jzb
+//
+//  Created by qzp on 15/5/5.
+//  Copyright (c) 2015年 qzp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
